7f27cef6fda1fed49a4cc05dc6e7c81ee0f7556c
Sabrina Fletcher <sfletc01@gmail.com>
2018-07-11 21:46:47 -0400

    Interfaces added and Implemented using AmexCC class

---
+++ EmployeeLibrary/AmexCC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    class AmexCC : CreditCard
+    {
+        public string creditCardNum { get; }
+
+        public string cvv { get; }
+
+        public string pin { get; set; }
+
+        public int Offer(int creditScore)
+        {
+            int discountedFee = 0;
+            if ((creditScore >= 650) && (creditScore < 700))
+            {
+                discountedFee = 15;
+            }
+            else if (creditScore > 700)
+            {
+                discountedFee = 25;
+            }
+            else
+            {
+                discountedFee = 0;
+            }
+            return discountedFee;
+        }
+    }
+}
---
+++ EmployeeLibrary/CreditCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLibrary
+{
+    interface CreditCard
+    {
+        string creditCardNum { get; }
+        string cvv { get; }
+        string pin { get; set; }
+        int Offer(int creditScore);
+    }
+}
+
